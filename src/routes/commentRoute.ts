01ce13165c22587fb2e4f08b6d44e120c3c4a9fe
root <root@Admin-PC.localdomain>
2023-12-21 11:18:58 +0700

    fixed code

--- src/routes/commentRoute.ts
+++ src/routes/commentRoute.ts
@@ -1,44 +1,47 @@
-import express, { Request, Response } from 'express'
-import asyncHandler from 'express-async-handler'
-import { isAuth } from '../utils'
-import { CommentModel, IComment } from '../model/commentModel'
-import {  ProductModel } from '../model/productModel'
-export const commentRouter = express.Router()
+import express, { Request, Response } from "express";
+import asyncHandler from "express-async-handler";
+import { isAuth } from "../utils";
+import { CommentModel, IComment } from "../model/commentModel";
+import { ProductModel } from "../model/productModel";
+export const commentRouter = express.Router();
 
 commentRouter.get(
-  '/:slug',
+  "/:slug",
   asyncHandler(async (req: Request, res: Response) => {
-    const comments = await CommentModel.find({ slug: req.params.slug })
-     if (comments) {
+    const comments = await CommentModel.find({ slug: req.params.slug });
+    if (comments) {
       res.json(comments.reverse());
     } else {
-      res.status(404).json({ message: 'Comments Not Found' })
+      res.status(404).json({ message: "Comments Not Found" });
     }
   })
-)
+);
 
 commentRouter.post(
-  '/',
+  "/",
   isAuth,
   asyncHandler(async (req: Request, res: Response) => {
-   const comment =  new CommentModel({
-        username:req.body.username,
-        slug:req.body.slug,
-        text:req.body.text,
-        rating:req.body.rating,
-   } as IComment);
-      await comment.save();
+    const comment = new CommentModel({
+      username: req.body.username,
+      slug: req.body.slug,
+      text: req.body.text,
+      rating: req.body.rating,
+    } as IComment);
+    await comment.save();
 
-    const product = await ProductModel.findOne({slug:req.body.slug});
-    const reviews = await CommentModel.find({slug:req.body.slug});
-    if(product && reviews) {
+    const product = await ProductModel.findOne({ slug: req.body.slug });
+    const reviews = await CommentModel.find({ slug: req.body.slug });
+    if (product && reviews) {
       const count = reviews.length;
-      const sum = reviews.reduce((acc, review) => acc + review.rating, 0);
+      const sum = reviews.reduce(
+        (acc: any, review: any) => acc + review.rating,
+        0
+      );
       const avgRating = count > 0 ? sum / count : 0;
       product.numReviews = count;
       product.rating = avgRating;
       await product.save();
-    } 
-    res.status(201).json({ message: 'Comment Created', product})
+    }
+    res.status(201).json({ message: "Comment Created", product });
   })
-)
+);
